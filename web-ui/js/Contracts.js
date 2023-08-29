ab035540233f9e430d0cb28aa296cee8756c8f64
HansLuft778 <hendrik.lante@gmail.com>
2023-08-30 01:06:28 +0200

    added swal2 + contracts page

--- web-ui/js/Contracts.js
+++ web-ui/js/Contracts.js
@@ -1,13 +1,111 @@
-function getContracts() {
+
+
+async function getContracts() {
+
+    const APIkey = sessionStorage.getItem('api_key');
+
     const options = {
         headers: {
             'Content-Type': 'application/json',
-            'Authorization': 'Bearer INSERT_TOKEN_HERE'
+            'Authorization': 'Bearer ' + APIkey,
         },
     };
 
-    fetch('https://api.spacetraders.io/v2/my/contracts', options)
-        .then(response => response.json())
-        .then(response => console.log(response))
-        .catch(err => console.error(err));
+    let result = await fetch('https://api.spacetraders.io/v2/my/contracts', options)
+        .then(response => response.json());
+
+    console.log(result);
+    displayContracts(result);
 }
+
+async function acceptContract(contractID, button) {
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + sessionStorage.getItem('api_key'),
+        },
+    };
+
+    const result = await fetch('https://api.spacetraders.io/v2/my/contracts/cllvhzpant1vos60c4x8qflau/accept', options)
+        .then(response => response.json());
+
+    console.log(result);
+
+    if (result.error) {
+        Swal.fire({
+            title: 'Error!',
+            text: result.error.message,
+            icon: 'error',
+            confirmButtonText: 'Shit'
+        })
+        return;
+    }
+
+    updateButtonStatus(button);
+}
+
+function updateButtonStatus(button) {
+    button.innerHTML = "Accepted";
+    button.className = "btn btn-success";
+}
+
+function displayContracts(rawData) {
+    const contractsDiv = document.getElementById("contractList");
+    if (!contractsDiv) {
+        return;
+    }
+
+    rawData.data.forEach(contract => {
+        const cardDiv = document.createElement("div");
+        cardDiv.className = "card";
+        cardDiv.setAttribute("style", "width: 18rem; margin-top: 10px;")
+
+        const cardBodyDiv = document.createElement("div");
+        cardBodyDiv.className = "card-body";
+
+        const cardTitle = document.createElement("h5");
+        cardTitle.className = "card-title";
+        cardTitle.innerHTML = contract.type;
+
+        const cardSubtitleSec = document.createElement("h6");
+        cardSubtitleSec.className = "card-subtitle mb-2 text-body-secondary";
+        var date = new Date(contract.expiration);
+        cardSubtitleSec.innerHTML = date.toLocaleString("de-DE");
+
+        const cardSubtitleSec2 = document.createElement("h6");
+        cardSubtitleSec2.className = "card-subtitle mb-2 text-body-secondary";
+        const pay = contract.terms.payment.onAccepted + contract.terms.payment.onFulfilled;;
+        cardSubtitleSec2.innerHTML = "Payment: $" + pay;
+
+        const cardSubtitleTer = document.createElement("h6");
+        cardSubtitleTer.className = "card-subtitle mb-2 text-body-tertiary";
+        cardSubtitleTer.innerHTML = contract.id;
+
+        const cardText = document.createElement("p");
+        cardText.className = "card-text";
+        const deliver = contract.terms.deliver[0];
+        cardText.innerHTML = "Deliver " + deliver.unitsRequired + " " + deliver.tradeSymbol + " to " + deliver.destinationSymbol;
+
+        const acceptButton = document.createElement("button");
+        acceptButton.className = contract.accepted ? "btn btn-success disabled" : "btn btn-primary";
+        acceptButton.innerHTML = contract.accepted ? "Accepted" : "Accept";
+        acceptButton.onclick = function () {
+            acceptContract(contract.id, acceptButton);
+        }
+
+        cardBodyDiv.appendChild(cardTitle);
+        cardBodyDiv.appendChild(cardSubtitleSec);
+        cardBodyDiv.appendChild(cardSubtitleSec2);
+        cardBodyDiv.appendChild(cardSubtitleTer);
+        cardBodyDiv.appendChild(cardText);
+        cardBodyDiv.appendChild(acceptButton);
+
+        cardDiv.appendChild(cardBodyDiv);
+        contractsDiv.appendChild(cardDiv);
+
+    });
+
+}
+
+getContracts();
--- web-ui/js/util.js
+++ web-ui/js/util.js
@@ -1,7 +1,34 @@
 function setAPIkey() {
     const apiKeyInput = document.getElementById("apiKeyInput");
     const key = apiKeyInput.value;
+
+    if (key === null || key === undefined || key === "" || key.length < 10) {
+        console.log('API key is not valid');
+        sessionStorage.removeItem('api_key');
+        checkAPIKeyIsSet();
+        return;
+    }
+
     // set the API key for the current session
     console.log('setting API key to ' + key);
     sessionStorage.setItem('api_key', key);
+
+    checkAPIKeyIsSet();
 }
+
+function checkAPIKeyIsSet() {
+    const isSetText = document.getElementById("APIkeySet");
+    const key = sessionStorage.getItem('api_key');
+
+    if (key === null) {
+        console.log('API key not set ❌');
+        isSetText.innerHTML = "API key not set ❌";
+        return false;
+    } else {
+        console.log('API key is set ✅');
+        isSetText.innerHTML = "API key is set ✅";
+        return true;
+    }
+}
+
+checkAPIKeyIsSet();
