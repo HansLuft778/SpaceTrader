faf2de720931740cab4597cb2bd71647a47912e9
HansLuft778 <hendrik.lante@gmail.com>
2023-08-30 11:52:13 +0200

    update gitignore

--- web-ui/js/APIkeyManager.js
+++ web-ui/js/APIkeyManager.js
@@ -41,5 +41,5 @@ export function setIsKeySet(value) {
     isKeySet = value;
     setAPIkeyMessage();
 }
-sessionStorage.setItem('api_key', '***REMOVED***');
+sessionStorage.setItem('api_key', 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZGVudGlmaWVyIjoiR09SRyIsInZlcnNpb24iOiJ2MiIsInJlc2V0X2RhdGUiOiIyMDIzLTA4LTI2IiwiaWF0IjoxNjkzMjUyOTA2LCJzdWIiOiJhZ2VudC10b2tlbiJ9.wES8blc6BS7FOlKXVmfGTV7xJ-pWOsw2VBPQY4HkdEI3sXrBsyE2ajNtJ9vPuujmr1haF7N4jIlQjY5bRXd06PYq2FSKlFG0ctTiIL8x6qe4KyS-NeQCucVzkb2iqzpJjbPZ5OJMsYiEh--Sx5BY3wmwqVTsjDMhO513JXL7UK6-Otb2xXegQ8vtI-J0JPMbP_w10ZxbhkWnJOC3IdRoVkqOAWv4gognaxgkIbY9Lj1jfVW-hdeKHaOVnd9EU-gAdBbkiY1LifrxTldixSUR_JCBCAPFbJBlqBoyPgoQ0I3p9guB6R8_tfrOb4OqcyLk2CGvTwX2fgjoW8p-LE9d6A');
 setAPIkeyMessage();
