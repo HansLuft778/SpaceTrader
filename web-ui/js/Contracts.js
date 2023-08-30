31069cbb3da8d8d41f586f8524bf408b36210a88
HansLuft778 <hendrik.lante@gmail.com>
2023-08-30 11:50:38 +0200

    new stuff

--- web-ui/js/Contracts.js
+++ web-ui/js/Contracts.js
@@ -1,4 +1,4 @@
-
+import { Card } from "./ContractCard.js";
 
 async function getContracts() {
     const options = {
@@ -54,55 +54,14 @@ function displayContracts(rawData) {
     }
 
     rawData.data.forEach(contract => {
-        const cardDiv = document.createElement("div");
-        cardDiv.className = "card";
-        cardDiv.setAttribute("style", "width: 18rem; margin-top: 10px;")
+        const card = new Card(contract);
+        const cardDiv = card.renderCard(contractsDiv);
 
-        const cardBodyDiv = document.createElement("div");
-        cardBodyDiv.className = "card-body";
-
-        const cardTitle = document.createElement("h5");
-        cardTitle.className = "card-title";
-        cardTitle.innerHTML = contract.type;
-
-        const cardSubtitleSec = document.createElement("h6");
-        cardSubtitleSec.className = "card-subtitle mb-2 text-body-secondary";
-        var date = new Date(contract.expiration);
-        cardSubtitleSec.innerHTML = date.toLocaleString("de-DE");
-
-        const cardSubtitleSec2 = document.createElement("h6");
-        cardSubtitleSec2.className = "card-subtitle mb-2 text-body-secondary";
-        const pay = contract.terms.payment.onAccepted + contract.terms.payment.onFulfilled;;
-        cardSubtitleSec2.innerHTML = "Payment: $" + pay;
-
-        const cardSubtitleTer = document.createElement("h6");
-        cardSubtitleTer.className = "card-subtitle mb-2 text-body-tertiary";
-        cardSubtitleTer.innerHTML = contract.id;
-
-        const cardText = document.createElement("p");
-        cardText.className = "card-text";
-        const deliver = contract.terms.deliver[0];
-        cardText.innerHTML = "Deliver " + deliver.unitsRequired + " " + deliver.tradeSymbol + " to " + deliver.destinationSymbol;
-
-        const acceptButton = document.createElement("button");
-        acceptButton.className = contract.accepted ? "btn btn-success disabled" : "btn btn-primary";
-        acceptButton.innerHTML = contract.accepted ? "Accepted" : "Accept";
+        const acceptButton = cardDiv.getElementsByTagName("button")[0];
         acceptButton.onclick = function () {
             acceptContract(contract.id, acceptButton);
         }
-
-        cardBodyDiv.appendChild(cardTitle);
-        cardBodyDiv.appendChild(cardSubtitleSec);
-        cardBodyDiv.appendChild(cardSubtitleSec2);
-        cardBodyDiv.appendChild(cardSubtitleTer);
-        cardBodyDiv.appendChild(cardText);
-        cardBodyDiv.appendChild(acceptButton);
-
-        cardDiv.appendChild(cardBodyDiv);
-        contractsDiv.appendChild(cardDiv);
-
     });
-
 }
 
 getContracts();
